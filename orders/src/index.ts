e6dc9fbcf5719af81edf187e7e5a48c29454e247
tianbo <tianbo152@gmail.com>
2023-05-29 14:57:45 +0800

    add startup message to orders service

--- orders/src/index.ts
+++ orders/src/index.ts
@@ -8,6 +8,8 @@ import { ExpirationCompleteListener } from './events/listener/expiration-complet
 import { PaymentCreatedListener } from './events/listener/payment-created-listener';
 
 const start = async () => {
+  console.log("Staring up ...");
+  
   if (!process.env.JWT_KEY) {
     throw new Error('JWT_KEY MUST BE DEFINED');
   }
